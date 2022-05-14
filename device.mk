0f6fec4491c87e3fa935e91beda7b9a56ea8eda4
iusmac <iusico.maxim@libero.it>
2022-05-14 11:25:45 +0200

    rova: Build dependency libs
    Reference: https://github.com/TeamWin/android_device_asus_sm8350-common/blob/3a322e37c0d204d85a211ef582885e8d785d506f/BoardConfigCommon.mk#L124
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- device.mk
+++ device.mk
@@ -15,6 +15,10 @@
 # limitations under the License.
 #
 
+# Apex libraries
+PRODUCT_COPY_FILES += \
+    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
+
 # Encryption
 PRODUCT_PACKAGES += \
     libcryptfs_hw \
