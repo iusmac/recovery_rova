0d46797a6457632cb90a9b479a452b2306e55ae8
iusmac <iusico.maxim@libero.it>
2022-06-11 12:21:14 +0200

    rova: patches: Reduce battery drain due to progress thread running all the time
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- patches/apply.sh
+++ patches/apply.sh
@@ -6,7 +6,8 @@ function main() {
     for patch in \
         bootable_recovery-Fix-keymaster-on-A12.patch \
         bootable_recovery-Add-FBE-alert.patch \
-        bootable_recovery-Fix-double-bind-mounting-data-media.patch
+        bootable_recovery-Fix-double-bind-mounting-data-media.patch \
+        bootable_recovery-Fix-the-progress-bar.patch
     do
         file="ofox-11.0/$patch"
 
