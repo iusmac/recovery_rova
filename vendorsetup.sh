ea8c4a1f795c7793ce88eb2f294912eff59c876c
iusmac <iusico.maxim@libero.it>
2022-11-20 17:20:02 +0100

    Revert "rova: vendorsetup: Enable OTA by default"
    This reverts commit 9e2f4ced82f3045e0c1d0345a8655acf03fc43f4.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -67,6 +67,7 @@ export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR='1'
 export FOX_VERSION='R11.1_5'
 export OF_MAINTAINER=iusmac
 export OF_USE_TWRP_SAR_DETECT='1'
+export OF_DISABLE_MIUI_OTA_BY_DEFAULT='1'
 export OF_QUICK_BACKUP_LIST='/system_root;/vendor;/data;/persist;/boot;'
 export FOX_VARIANT='FDE+FBE'
 
