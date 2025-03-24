664d703c4330209474b1c7f1ce74f77db289a8c7
iusmac <iusico.maxim@libero.it>
2025-03-24 19:08:34 +0100

    patches: Remove `Save screenshots to /tmp/ if sdcard is not mounted` patch
    Already implemented in the upstream sources.
    
    Signed-off-by: iusmac <iusico.maxim@libero.it>

--- patches/apply.sh
+++ patches/apply.sh
@@ -5,7 +5,6 @@
 function main() {
     for patch in \
         bootable_recovery-Fix-the-progress-bar.patch \
-        bootable_recovery-Save-screenshot-to-tmp-screenshots-if-sdcard-is-not-mounted.patch \
         bootable_recovery-data-Concatenate-battery-temperature-with-CPU-temperature.patch \
         bootable_recovery-Revert-gui.cpp-Only-report-touchscreen-input-when-sc.patch \
         # bootable_recovery-Copy-fox-persist-dependencies-to-ramdisk.patch
